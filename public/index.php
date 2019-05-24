c47c1da4ea5ad5d72641241264d843e84e25d88d
skarpov <sergo-8ck@yandex.ru>
2019-05-24 15:38:09 +0300

    Сеттеры

--- public/index.php
+++ public/index.php
@@ -7,11 +7,17 @@ require 'vendor/autoload.php';
 
 ### Initialization
 
-$request = new Request($_GET, $_POST);
+//$request = new Request();
+//$request->withQueryParams($_GET);
+//$request->withParsedBody($_POST);
+
+// необходим return $this в каждом из методов
+$request = (new Request())
+    ->withQueryParams($_GET)
+    ->withParsedBody($_POST);
 
 ### Action
 
 $name = $request->getQueryParams()['name'] ?? 'Guest';
-
 header('X-Developer: Sergo8ck');
 echo 'Hello, ' . $name . '!';
--- src/Framework/Http/Request.php
+++ src/Framework/Http/Request.php
@@ -7,19 +7,26 @@ class Request
     private $queryParams;
     private $parsedBody;
 
-    public function __construct(array $queryParams = [], $paresdBody = null)
-    {
-        $this->queryParams = $queryParams;
-        $this->parsedBody = $paresdBody;
-    }
-
     public function getQueryParams(): array
     {
         return $this->queryParams;
+    }
+
+    public function withQueryParams(array $query)
+    {
+        $this->queryParams = $query;
+        return $this;
     }
 
     public function getParsedBody()
     {
         return $this->parsedBody ?: null;
     }
+
+    public function withParsedBody($data)
+    {
+        $this->parsedBody = $data;
+        return $this;
+    }
+
 }
