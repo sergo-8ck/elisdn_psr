323cd7ccd6d2c3756bc6cc315e1425a6d520f4a6
skarpov <sergo-8ck@yandex.ru>
2019-05-27 19:23:49 +0300

    Router

--- public/index.php
+++ public/index.php
@@ -1,71 +1,65 @@
 <?php
 
+use Framework\Http\Router\Exception\RequestNotMatchedException;
+use Framework\Http\Router\RouteCollection;
+use Framework\Http\Router\Router;
 use Psr\Http\Message\ServerRequestInterface;
 use Zend\Diactoros\Response\HtmlResponse;
+use Zend\Diactoros\Response\JsonResponse;
 use Zend\HttpHandlerRunner\Emitter\SapiEmitter;
 use Zend\Diactoros\ServerRequestFactory;
-use Zend\Diactoros\Response\JsonResponse;
 
 chdir(dirname(__DIR__));
 require 'vendor/autoload.php';
 
 ### Initialization
 
+$routes = new RouteCollection();
+
+$routes->get('home', '/', function (ServerRequestInterface $request) {
+    $name = $request->getQueryParams()['name'] ?? 'Guest';
+    return new HtmlResponse('Hello, ' . $name . '!');
+});
+
+$routes->get('about', '/about', function () {
+    return new HtmlResponse('I am a simple site');
+});
+
+$routes->get('blog', '/blog', function () {
+    return new JsonResponse([
+        ['id' => 2, 'title' => 'The Second Post'],
+        ['id' => 1, 'title' => 'The First Post'],
+    ]);
+});
+
+$routes->get('blog_show', '/blog/{id}', function (ServerRequestInterface $request) {
+    $id = $request->getAttribute('id');
+    if ($id > 2) {
+        return new HtmlResponse('Undefined page', 404);
+    }
+    return new JsonResponse(['id' => $id, 'title' => 'Post #' . $id]);
+}, ['id' => '\d+']);
+
+$router = new Router($routes);
+
+### Running
+
 $request = ServerRequestFactory::fromGlobals();
-
-### Preprocessing
-// TODO
-
-### Action
-
-$path = $request->getUri()->getPath();
-$action = null;
-
-if ($path === '/') {
-
-    $action = function (ServerRequestInterface $request) {
-        $name = $request->getQueryParams()['name'] ?? 'Guest';
-        return new HtmlResponse('Hello, ' . $name . '!');
-    };
-
-} elseif ($path === '/about') {
-
-    $action = function () {
-        return new HtmlResponse('<h1>About</h1>');
-    };
-
-} elseif ($path === '/blog') {
-
-    $action = function () {
-        return new JsonResponse([
-            ['id' => 2, 'title' => 'The Second Post'],
-            ['id' => 1, 'title' => 'The First Post']
-        ]);
-    };
-
-} elseif (preg_match('#^/blog/(?<id>\d+)$#i', $path, $matches)) {
-
-    $request = $request->withAttribute('id', $matches['id']);
-
-    $action = function (ServerRequestInterface $request) {
-      $id = $request->getAttribute('id');
-      if ($id > 2) {
-          return new JsonResponse(['error' => 'Undefined page'], 404);
-      }
-      return new JsonResponse(['id' => $id, 'title' => 'Post #' . $id]);
-    };
-
-}
-
-if ($action) {
+try {
+    $result = $router->match($request);
+    foreach ($result->getAttributes() as $attribute => $value) {
+        $request = $request->withAttribute($attribute, $value);
+    }
+    /** @var callable $action */
+    $action = $result->getHandler();
     $response = $action($request);
-} else {
-    $response = new JsonResponse(['error' => 'Undefined page'], 404);
+} catch (RequestNotMatchedException $e){
+    $response = new HtmlResponse('Undefined page', 404);
 }
 
 ### Postprocessing
 
-$response->withHeader('X-Developer', 'Sergo8ck');
+$response = $response->withHeader('X-Developer', 'ElisDN');
 
 ### Sending
 
---
+++ src/Framework/Http/Router/Exception/RouteNotFoundException.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Framework\Http\Router;
+
+
+class RouteNotFoundException extends \LogicException
+{
+    private $name;
+    private $params;
+
+    /**
+     * RouteNotFoundException constructor.
+     *
+     * @param       $name
+     * @param array $params
+     */
+    public function __construct($name, array $params)
+    {
+        parent::__construct('Route "' . $name . '" not found.');
+        $this->name = $name;
+        $this->params = $params;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getParams(): array
+    {
+        return $this->params;
+    }
+
+}
