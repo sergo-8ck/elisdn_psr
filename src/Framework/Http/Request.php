d6040493ae6909de022cd3638ad80109e18f90d4
skarpov <sergo-8ck@yandex.ru>
2019-05-27 12:37:22 +0300

    Added PSR7 interface

--- src/Framework/Http/Request.php
+++ src/Framework/Http/Request.php
@@ -2,6 +2,10 @@
 
 namespace Framework\Http;
 
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\StreamInterface;
+use Psr\Http\Message\UriInterface;
+
 class Request implements ServerRequestInterface
 {
     private $queryParams;
@@ -27,7 +31,7 @@ class Request implements ServerRequestInterface
 
     public function getParsedBody()
     {
-        return $this->parsedBody ?: null;
+        return $this->parsedBody;
     }
 
     public function withParsedBody($data): self
@@ -37,4 +41,30 @@ class Request implements ServerRequestInterface
         return $new;
     }
 
+    public function getProtocolVersion() {}
+    public function withProtocolVersion($version) {}
+    public function getHeaders() {}
+    public function hasHeader($name) {}
+    public function getHeader($name) {}
+    public function getHeaderLine($name) {}
+    public function withHeader($name, $value) {}
+    public function withAddedHeader($name, $value) {}
+    public function withoutHeader($name) {}
+    public function getBody() {}
+    public function withBody(StreamInterface $body) {}
+    public function getRequestTarget() {}
+    public function withRequestTarget($requestTarget) {}
+    public function getMethod() {}
+    public function withMethod($method) {}
+    public function getUri() {}
+    public function withUri(UriInterface $uri, $preserveHost = false) {}
+    public function getServerParams() {}
+    public function getCookieParams() {}
+    public function withCookieParams(array $cookies) {}
+    public function getUploadedFiles() {}
+    public function withUploadedFiles(array $uploadedFiles) {}
+    public function getAttributes() {}
+    public function getAttribute($name, $default = null) {}
+    public function withAttribute($name, $value) {}
+    public function withoutAttribute($name) {}
 }
