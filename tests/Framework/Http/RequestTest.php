9837175760af94ffc237cc520d551694d5bd1f8b
skarpov <sergo-8ck@yandex.ru>
2019-05-27 13:09:38 +0300

    Installed Zend Diactoros

--- tests/Framework/Http/RequestTest.php
+++ tests/Framework/Http/RequestTest.php
@@ -2,14 +2,14 @@
 
 namespace Test\Framework\Http;
 
-use Framework\Http\Request;
 use PHPUnit\Framework\TestCase;
+use Zend\Diactoros\ServerRequest;
 
 class RequestTest extends TestCase
 {
     public function testEmpty(): void
     {
-        $request = new Request();
+        $request = new ServerRequest();
 
         self::assertEquals([], $request->getQueryParams());
         self::assertNull($request->getParsedBody());
@@ -17,7 +17,7 @@ class RequestTest extends TestCase
 
     public function testQueryParams(): void
     {
-        $request = (new Request())
+        $request = (new ServerRequest())
             ->withQueryParams($data = [
                 'name' => 'John',
                 'age' => 20
@@ -29,7 +29,7 @@ class RequestTest extends TestCase
 
     public function testParseBody(): void
     {
-        $request = (new Request())
+        $request = (new ServerRequest())
             ->withParsedBody($data = ['title' => 'Title']);
 
         self::assertEquals([], $request->getQueryParams());
